114a01d91687d60dba3b75261dc078a9f3ea5a35
josemazhuvanchery1 <jose1998mathew@gmail.com>
2022-03-23 18:14:48 +0000

    Question 2 solved

--- index.js
+++ index.js
@@ -1,7 +1,19 @@
 //Code your solutions in this file
 function fiveToHundred(){
+    console.log("** Question 1 **")
     for (let i =5; i <=100;i++){
         console.log(i)
     }
 }
 fiveToHundred()
+
+// 2
+function multiplesOfThree(){
+    console.log("** Question 2 **")
+    for(let i=1; i<= 100; i++){
+        if(i%3 === 0){
+            console.log(i);
+        }
+    }
+}
+multiplesOfThree();
